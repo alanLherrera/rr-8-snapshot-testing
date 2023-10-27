264ef94f8314e44944d6d6953fb16e2138e6532e
alanLherrera <108435763+alanLherrera@users.noreply.github.com>
2023-10-27 12:52:33 -0700

    corrected the issues

--- snapshot-tests/src/GitHubCard/GitHubCard.js
+++ snapshot-tests/src/GitHubCard/GitHubCard.js
@@ -1,19 +1,18 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import MickeyMouse from "./Mickey_Mouse.png";
+import Jake from "./jake.jpeg";
 
 function GitHubCard() {
   return (
     <div className="card">
       <Card style={{ width: "18rem" }}>
         {/* Use your GitHub info instead, but Mickey Mouse is used as an example */}
-        <Card.Img variant="top" src={MickeyMouse} />
+        <Card.Img variant="top" src={Jake} />
         {/* Source: https://en.wikipedia.org/wiki/Mickey_Mouse */}
         <Card.Body>
-          <Card.Title>Mickey Mouse</Card.Title>
+          <Card.Title>Jake The Dog</Card.Title>
           <Card.Text>
-            I am a cartoon character and am one of the world's most recognizable
-            fictional characters.
+            whats up! yall its jake the dog from adventure time how yall doin
           </Card.Text>
         </Card.Body>
       </Card>
